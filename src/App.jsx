98c3724aaa705d83352bfad7ed467f66112757d9
kogilo <getabullaothow@gmail.com>
2024-10-28 21:55:50 -0500

    Fisrt Comment

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content() {
+  return (<p>This is my first React Application, wish me luck!</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
